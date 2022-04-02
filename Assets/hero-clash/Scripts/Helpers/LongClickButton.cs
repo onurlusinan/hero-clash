232f41d35ef5b4cba74b859a91e2fced7c9eee56
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2022-04-02 15:35:50 +0300

    forgot to delete a line

--- Assets/hero-clash/Scripts/Helpers/LongClickButton.cs
+++ Assets/hero-clash/Scripts/Helpers/LongClickButton.cs
@@ -44,7 +44,6 @@ public class LongClickButton : MonoBehaviour,  IPointerUpHandler, IPointerDownHa
         if(!eventData.dragging)
             hero.CharacterPressed();
         
-        if(eventData.clickTime < 3)
             Reset();
     }
 
